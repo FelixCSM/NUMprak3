3719b92bf203335f82a82ffd6f30731ac5b17067
FelixCSM <felix-voelker@web.de>
2016-07-05 16:39:03 +0200

    BUGFIX: function is a instruction cannot be used as parameter, refactored to function_to_use

--- rungeKuttaMethod.m
+++ rungeKuttaMethod.m
@@ -1,9 +1,9 @@
-function u_i = rungeKuttaMethod(function, functionparameter, boundries, stepsize, u_i)
+function u_i = rungeKuttaMethod(function_to_use, functionparameter, boundries, stepsize, u_i)
   for t = boundries(1):boundries(2):stepsize
-    k_1 = function(functionparameter, t,                u_i                       )
-    k_2 = function(functionparameter, t + stepsize/2.0, u_i + stepsize * k_1 / 2.0)
-    k_3 = function(functionparameter, t + stepsize/2.0, u_i + stepsize * k_2 / 2.0)
-    k_4 = function(functionparameter, t + stepsize,     u_i + stepsize * k_3      )
-    u_i = u_i + stepsize * ( k_1/6.0 + k_2/3.0 + k_3/3.0 + k_4/6.0)
+    k_1 = function_to_use(functionparameter, t,                u_i                       );
+    k_2 = function_to_use(functionparameter, t + stepsize/2.0, u_i + stepsize * k_1 / 2.0);
+    k_3 = function_to_use(functionparameter, t + stepsize/2.0, u_i + stepsize * k_2 / 2.0);
+    k_4 = function_to_use(functionparameter, t + stepsize,     u_i + stepsize * k_3      );
+    u_i = u_i + stepsize * ( k_1/6.0 + k_2/3.0 + k_3/3.0 + k_4/6.0);
   endfor
 endfunction
