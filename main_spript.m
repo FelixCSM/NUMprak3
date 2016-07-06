7d24656dd1e7143e861465d71dc9246d5e367d46
FelixCSM <felix-voelker@web.de>
2016-07-06 12:21:35 +0200

    extension for convegence

--- main_spript.m
+++ main_spript.m
@@ -1,32 +1,36 @@
 #! /bin/octave
-interval = [0,10]
+interval = [0, 10];
 ########################################################################
 ### task c)
 function_to_analyse = @awp_1
 y_of_0 = 0 #y(0)=0
+analytical_matrix = [];
 for a = [-10, 1]
   for tau_maker = 1:12
     tau = 2.0**(-(tau_maker-1));
-    disp(["a ist ", a, " Schrittweite ist", tau])
-    disp("Ergebnis des expliziten Eulerverfahrens")
-    ret_y = explicitEulerMethod(function_to_analyse, a, interval, tau, y_of_0)
-    disp("Ergebnis des Runge-Kutta-Verfahrens ist")
-    ret_y = rungeKuttaMethod(function_to_analyse, a, interval, tau, y_of_0)
+    y_exp(1:2) = explicitEulerMethod(function_to_analyse, a, interval, tau, y_of_0);
+    y_rk4(1:2) = rungeKuttaMethod(function_to_analyse, a, interval, tau, y_of_0);
+    numerische_konvergenz_exp = norm(y_exp(1) - sin(interval(2)))/norm(y_exp(1) - sin(interval(2)));
+    numerische_konvergenz_rk4 = norm(y_rk4(1) - sin(interval(2)))/norm(y_rk4(1) - sin(interval(2)));
+    disp(y_exp(1))
+    disp(y_exp(2))
+    analytical_matrix(tau_maker) = [tau, y_exp(1), y_exp(2), numerische_konvergenz_exp, y_rk4(1), y_rk4(2), numerische_konvergenz_rk4];
   endfor
 endfor
+disp(analytical_matrix)
 
 ########################################################################
 ### task d)
 function_to_analyse = @awp_2
-alpha = 100
-y_of_0 = alpha
-beta = 1
+alpha = 100;
+y_of_0 = alpha;
+beta = 1;
 taus = [0.05];
 for i = 1:5
   taus(i+1) = taus(i)/2.0;
 endfor
 for tau = taus
-  disp(["beta = ", beta, "; alpha = ", alpha, "; Tau = ", tau])
+  disp(["beta = ", num2str(beta), "; alpha = ", num2str(alpha), "; Tau = ", num2str(tau)])
     disp("Ergebnis des expliziten Eulerverfahrens")
     ret_y = explicitEulerMethod(function_to_analyse, beta, interval, tau, y_of_0)
     disp("Ergebnis des Runge-Kutta-Verfahrens ist")
--- rungeKuttaMethod.m
+++ rungeKuttaMethod.m
@@ -1,5 +1,6 @@
-function u_i = rungeKuttaMethod(function_to_use, functionparameter, boundries, stepsize, u_i)
+function [u_i, u_im1] = rungeKuttaMethod(function_to_use, functionparameter, boundries, stepsize, u_i)
   for t = boundries(1):boundries(2):stepsize
+    u_im1 = u_i;
     k_1 = function_to_use(functionparameter, t,                u_i                       );
     k_2 = function_to_use(functionparameter, t + stepsize/2.0, u_i + stepsize * k_1 / 2.0);
     k_3 = function_to_use(functionparameter, t + stepsize/2.0, u_i + stepsize * k_2 / 2.0);
